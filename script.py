bfcba7aa4a26f9d45274a4fda86e9cf9606a3eda
Catarina Aguiar <123583987+CatarinaAguiar3@users.noreply.github.com>
2026-05-03 16:18:26 -0300

    :sparkles: Feat: Alteração no script.

--- script.py
+++ script.py
@@ -5,10 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
-# env
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Configurar o driver do Selenium (Chrome)
@@ -16,15 +15,16 @@ options = webdriver.ChromeOptions()
 # Descomente a linha abaixo para modo headless (sem abrir a janela do navegador)
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
 # Oculta mensagens de erro do console (como o DEPRECATED_ENDPOINT)
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 # Define o nível de log para mostrar apenas erros fatais
-options.add_argument("--log-level=3") 
+options.add_argument("--log-level=3")
 
 navegador = webdriver.Chrome(
     service=Service(ChromeDriverManager().install()), options=options
 )
-
 navegador.get(os.getenv("SITE"))
 espera_maxima = WebDriverWait(navegador, 10)
 
@@ -39,12 +39,10 @@ try:
     )
     input_password.send_keys(os.getenv("PASSWORD"))
 
-    botao_login = espera_maxima.until(
-        EC.element_to_be_clickable((By.ID, "Login"))
-    )
+    botao_login = espera_maxima.until(EC.element_to_be_clickable((By.ID, "Login")))
     botao_login.click()
     print("Login realizado com sucesso.")
-    time.sleep(20) 
+    time.sleep(20)
 
     botao_atualizar = espera_maxima.until(
         EC.element_to_be_clickable((By.XPATH, "//button[@title='Atualizar']"))
@@ -54,14 +52,24 @@ try:
     time.sleep(60)
 
     try:
+        # botao_assumir = espera_maxima.until(
+        #     EC.element_to_be_clickable(
+        #         (
+        #             By.XPATH,
+        #             "//button[@title='Assumir'] | //a[contains(text(), 'Assumir') and @href]",
+        #         )
+        #     )
+        # )
+
         botao_assumir = espera_maxima.until(
-             EC.element_to_be_clickable((By.XPATH, 
-            "//button[@title='Assumir'] | //a[contains(text(), 'Assumir') and @href]"
-        )))
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, "button.slds-button.slds-button_success.slds-button_stretch")
+            )
+        )
 
         # Procura qualquer elemento (*) que tenha "Assumir" no título ou texto
         # botao_assumir = espera_maxima.until(
-        #     EC.element_to_be_clickable((By.XPATH, 
+        #     EC.element_to_be_clickable((By.XPATH,
         #         "//*[@title='Assumir' or contains(text(), 'Assumir')]"
         #     ))
         # )
